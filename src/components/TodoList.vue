ae9c49435eaeb76d7a1a53e1c1619efe5b322357
micci184 <micci184@gmail.com>
2023-10-04 09:50:28 +0900

    add Todos.vue

---
+++ src/components/TodoList.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+<template>
+  <div class="container">
+    <h1>Todos</h1>
+    <Todos />
+  </div>
+</template>
+<style>
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+<template>
+	<v-app>
+		<v-main>
+      <v-container class="bg-grey-lighten-3 mx-2">
+
+				<v-sheet width="500px" class="bg-white my-4">
+					<v-text-field label="Add new.." hide-details="auto" class="bg-white">
+						<!-- <template #append> -->
+							<!-- <v-btn color="light-blue-accent-3">Add</v-btn> -->
+						<!-- </template> -->
+					</v-text-field>
+				</v-sheet>
+				</v-container>
+		</v-main>
+	</v-app>
+</template>
+<style>
+</style>
