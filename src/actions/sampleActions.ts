a40131d5c10935a3aa81520f5b821328da76e0fe
micci184 <micci184@gmail.com>
2024-07-15 13:16:07 +0900

    mod

--- src/actions/sampleActions.ts
+++ src/actions/sampleActions.ts
@@ -1,8 +1,18 @@
 "use server";
 
-export const createTask = async (taskId: number, formData: FormData) => {
+interface FormState {
+  error: string;
+}
+
+export const createTask = async (
+  taskId: number,
+  state: FormState,
+  formData: FormData
+) => {
   // DB にタスクを作成する
   console.log("Create Task!");
-	console.log(formData.get("name"));
-	console.log(taskId);
+  console.log(formData.get("name"));
+  console.log(taskId);
+  state.error = "Occured Error!";
+  return state;
 };
